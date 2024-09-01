4b9c50b78ecbf6cbf1fddbc0de14ac7a26052652
NikoGameing <niko@theminecraftpieceisreal.co.uk>
2024-09-01 17:49:11 +0100

    Removed unnecessary/duplicate files

--- src/main/java/nikosmods/weather2additions/blocks/blockreg/Blocks.java
+++ src/main/java/nikosmods/weather2additions/blocks/blockreg/Blocks.java
@@ -11,7 +11,7 @@ import nikosmods.weather2additions.blocks.SmallBatteryBlock;
 
 public class Blocks {
     private static final DeferredRegister<Block> blocks = DeferredRegister.create(ForgeRegistries.BLOCKS, Weather2Additions.MODID);
-    public static final RegistryObject<Block> SMALL_BATTERY_BLOCK = blocks.register("small_battery_block", () -> new SmallBatteryBlock(BlockBehaviour.Properties.of().noOcclusion()));
+    public static final RegistryObject<Block> SMALL_BATTERY_BLOCK = blocks.register("small_battery_block", () -> new SmallBatteryBlock(BlockBehaviour.Properties.copy(net.minecraft.world.level.block.Blocks.STONE)));
     public static void register(IEventBus modEventBus) {
         blocks.register(modEventBus);
     }
